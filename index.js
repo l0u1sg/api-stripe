a60e1f59275ee1ba203d71d236e0c8f3d25b7f34
Louis Gallet <louis.gallet@tumo.world>
2022-05-08 21:33:39 +0200

    feat: :construction:  Starting webhook route with stripe api and create .env

--- index.js
+++ index.js
@@ -28,6 +28,53 @@ app.post("/checkout", async (req, res) => {
 })
 
 
+// Listen to webhooks from Stripe when important events happen
+app.post('/webhook', async (req, res) => {
+    let data;
+    let eventType;
+    // Check if webhook signing is configured.
+    const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  
+    if (webhookSecret) {
+      // Retrieve the event by verifying the signature using the raw body and secret.
+      let event;
+      let signature = req.headers['stripe-signature'];
+  
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          signature,
+          webhookSecret
+        );
+      } catch (err) {
+        console.log(`⚠️  Webhook signature verification failed.`);
+        return res.sendStatus(400);
+      }
+      // Extract the object from the event.
+      data = event.data;
+      eventType = event.type;
+    } else {
+      // Webhook signing is recommended, but if the secret is not configured in `config.js`,
+      // retrieve the event data directly from the request body.
+      data = req.body.data;
+      eventType = req.body.type;
+    }
+  
+    switch (eventType) {
+      case 'checkout.session.completed':
+        console.log(data)
+        break;
+      case 'invoice.paid':
+        break;
+      case 'invoice.payment_failed':
+        break;
+      default:
+      // Unhandled event type
+    }
+  
+    res.sendStatus(200);
+  });
+
 app.listen(3000, () => {
     console.log("Server started on http://localhost:3000")
 })
